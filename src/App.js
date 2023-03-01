d7a2c1df6719956f834bc2cd7aec429ebdfd5f90
keyminy <imanaifs@gmail.com>
2023-03-02 00:16:47 +0900

    8,useMemo 사용

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 
 
 function App() {
-  const [data,setData] = useState(0);
-  const [search,setSearch] = useState(0);
+  const [list,setList] = useState([1,2,3,4]);
+  const [str,setStr] = useState('합계');
 
-  const download = () => {
-    //다운로드 받고(서버와 통신)
-    let downloadData = 5; //가정
-    setData(downloadData);
+
+  const getAddResult = () => {
+    let sum = 0;
+    list.forEach(i => sum += i)
+    console.log('sum함수 실행됬음 : ', sum);
+    return sum;
   }
-
-  useEffect(()=>{
-    console.log('useEffect실행됨');
-    download();
-  },[search]);
-
+  
+  const addResult = useMemo(()=>getAddResult(),[list]);
+  
   return (
     <div>
-      <button onClick={()=>{setSearch(2)}}>검색하기</button>
-      <h1>데이터 : {data}</h1>
-      <button onClick={()=>{setData(data+1)}}>더하기</button>
+      <button onClick={()=>{setStr('안녕')}}>문자 변경</button>
+      <button onClick={()=>{setList([...list,10])}}>리스트값 추가</button>
+      <div>
+        {list.map(i => {
+          return <h1>{i}</h1>;
+        })}
+      </div>
+      <div>{str} : {addResult}</div>
     </div>
   );
 }
